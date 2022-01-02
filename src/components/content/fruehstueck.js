0f70a98253b678d6f035ed3c804ff431ba4f5bba
Jay <me@aeiko.de>
2022-01-02 12:29:58 +0100

    add: früstücke seed, missing extra prices and formatting

--- src/components/content/fruehstueck.js
+++ src/components/content/fruehstueck.js
@@ -10,7 +10,55 @@ const fruehstuecke_speisen = [
         title: "Französisches Frühstück",
         description: "1 Heißgetränk nach Wahl, 1 Buttercroissant, hausgemachte Marmelade, Honig, Butter und ein Glas Saft (0,2l) Ihrer Wahl",
         price: "8.40"
+    },
+    {
+        id: 3,
+        title: "Sportivo (auch Veggie)",
+        description: "1 Heißgetränk nach Wahl, 1 Mehrkornbrötchen,  Frischkäse, Käse, Putenschinken1,  Rührei, frischer Obstsalat mit Naturjoghurt und Knuspermüsli",
+        price: "10.20"
+    },
+    {
+        id: 4,
+        title: "Classico (auch Veggie)",
+        description: "1 Heißgetränk nach Wahl, gemischter Brot- und Bröt-chenkorb, Butter, hausgemachte Marmelade, Honig, Geflügelaufschnitt, gekochter Schinken, Salami, Käse, Camembert und ein gekochtes Ei",
+        price: "12.10"
+    },
+    {
+        id: 5,
+        title: "Kids Frühstück",
+        description: "1 kleine heiße Schokolade oder geschäumte Milch,  1 Weizenbrötchen mit Nutella oder Marmelade",
+        price: "4.80"
     }, 
+    {
+        id: 6,
+        title: "Italiano",
+        description: "Heißgetränk nach Wahl, gemischter Brot- und Brötchenkorb, Butter, Frischkäse, Käse, luftgetrock-neter Schinken9, Weichkäse, Tomate-Mozzarella,  Salami6,9, Geflügel-Mortadella, Räucherlachs an Sahnemeerrettich6. Dazu servieren wir ein Glas  prickelnden Secco oder Saft Ihrer Wahl (0,1l)",
+        price: "16.10"
+    },
+    {
+        id: 7,
+        title: "EI-EI-Ei",
+        description: "1 Heißgetränk nach Wahl, 3 Rühreier mit  Gemüsewürfeln oder 3 Spiegeleier, 1 Weizen- brötchen und Butter",
+        price: "8.50"
+    }, 
+    {
+        id: 8,
+        title: "Lieblings-Schüssel (vegan)",
+        description: "Cremige, eiskalkte Fruchtbombe aus Banane, Waldbeeren und Mandelmilch, getoppt mit Leinsamen, Kokos",
+        price: "8.50"
+    },
+    {
+        id: 9,
+        title: "Teller-Kunterbunt (auch veggie)",
+        description: "1 Heißgetränk nach Wahl, knackigfrisches Gemüse-Allerlei (Motto: quer durch den Garten), Mehr-kornbrot, hausgemachter Frischkäse, Puten- schinken1, Scheibenkäse ",
+        price: "9.80"
+    },
+    {
+        id: 10,
+        title: "Kikeriki",
+        description: "1 Heißgetränk nach Wahl, 3 Rühreier mit frischen Kräutern, Käse und Tomaten auf zwei, mit Frischkäse  bestrichenen Scheiben Mehrkornbrot",
+        price: "9.10"
+    },            
 ]
 
 
