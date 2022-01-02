d9478ebceaae5e0cfd50987e85243041019ad51b
Jay <me@aeiko.de>
2022-01-02 13:01:32 +0100

    add: back button, not routed yet

---
+++ src/components/Buttons/BackButton/BackButton.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./BackButton.css"
+
+const BackButton = () => {
+    return(
+        <h2>Zurück</h2>
+    )
+}
+
+
+
+export default BackButton
--- src/components/pages/Fruehstueck.js
+++ src/components/pages/Fruehstueck.js
@@ -1,36 +1,46 @@
 import React from "react"
 import fruehstuecke_speisen from "../content/fruehstueck"
-
+import BackBtn from "../Buttons/BackButton/BackButton";
 
 
 const Fruehstueck = () => {
-    return (        
-
-
-<div className="container__fluid">
-
-           <h1 className="center">Frühstücke</h1>
-         <p className="speisen__subhead">In unseren Frühstücken ist jeweils ein Heißgetränk nach Wahl inklusive:Café Crema, Cappuccino, Milchkaffee, Latte Macchiato, Espresso, heiße Schokolade, Tee. Möchtest du lieber eine unserer Spezialitäten, berechnen wir einen Aufpreis von 50 Cent.</p>
-            
-
-                {
-                    fruehstuecke_speisen.map( (items) => {
-                        return (
-                        <div className="speisen__item">
-                        <h2 key="{title}">{ items.title }</h2>
-                        <p className="desc" key="{description}">{ items.description }</p>
-                        <p className="speisen__price" key="{price}">{ items.price }<span> EUR</span> </p>    
-                        </div>
-                        )
-                    })
-                }
-
-                      
-            
+	return (
+        <>
+        <header>
+            <div className="head__section">
+                <h1 className="center">Frühstücke</h1>
             </div>
+        </header>
+        <div className="back">
+            <BackBtn />
+        </div>
+		<div className="container__fluid">
+			
+			<p className="speisen__subhead">
+				In unseren Frühstücken ist jeweils ein Heißgetränk nach Wahl
+				inklusive:Café Crema, Cappuccino, Milchkaffee, Latte Macchiato,
+				Espresso, heiße Schokolade, Tee. Möchtest du lieber eine unserer
+				Spezialitäten, berechnen wir einen Aufpreis von 50 Cent.
+			</p>
 
-       
-    )
-}
+			{fruehstuecke_speisen.map((items) => {
+				return (
+					<div className="speisen__item">
+						<h2 key="{title}">{items.title}</h2>
+						<p className="desc" key="{description}">
+							{items.description}
+						</p>
+						<p className="speisen__price" key="{price}">
+							{items.price}
+							<span> EUR</span>{" "}
+						</p>
+					</div>
+				);
+			})}
+		</div>
+        </>
+	);
+    
+};
 
-export default Fruehstueck
+export default Fruehstueck;
