41e5aec548254feabac301b6812823ade732145f
Jay <me@aeiko.de>
2022-01-02 16:44:11 +0100

    add: footer

--- src/components/Buttons/ScrollToTop/ScrollToTop.js
+++ src/components/Buttons/ScrollToTop/ScrollToTop.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./ScrollToTop.css"
 
 const ScrollToTop = () => {
     return(
         <>
             <footer>
-            <p>Zurück nach oben</p>
+            <p  className="footer__content">Zurück nach oben</p>
             </footer>
         </>
     )
