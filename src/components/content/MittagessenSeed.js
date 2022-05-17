e48d5e17cc38cb41c32de22a30b59a643af565c6
Jay Milledge <jm@jay-media.de>
2022-05-17 21:40:31 +0200

    Update prices, change lunch

--- src/components/content/MittagessenSeed.js
+++ src/components/content/MittagessenSeed.js
@@ -1,10 +1,30 @@
 const mittagessen_speisen = [
   {
+    id: 5,
+    title: "Vital-Schüssel (gibts auch glutenfrei)",
+    description:
+      "Kunterbunte Bowl mit Allem was du brauchst. Grünzeug, Protein, Kohlenhydrate und Gemüse. Wir toppen deine Schüssel wahlweise mit: Hähnchenstreifen, Räucherlachs, Thunfisch, Feta und Ei oder vegan",
+    price: "11.50",
+  },
+  {
+    id: 6,
+    title: "Vital-Tasche",
+    description:
+      "Mit gesundem Allerlei gefüllter Weizentortilla, gegrillt. Wir toppen deinen Wrap wahlweise mit: Hähnchenstreifen, Räucherlachs, Thunfisch, Feta und Ei oder vegan",
+    price: "11.50",
+  },
+  {
+    id: 7,
+    title: "Kartoffelpizza",
+    description:
+      "Überbackener Reibekuchen wahlweise mit: Vegetarisch / Salami oder Schiken-Zwiebel",
+    price: "6.90",
+  },
+  {
     id: 1,
     title: "Currywurst (gibt´s auch vegan)",
-    description: "Rindswurst mit hausgemachten Soßen und gegrilltem Weißbrot",
-    price: "5.90",
-    alt_one: "vegan 6.90",
+    description: "Rindswurst mit hausgemachter Soße und Weißbrot",
+    price: "6.90",
   },
   {
     id: 2,
@@ -16,7 +36,7 @@ const mittagessen_speisen = [
     id: 3,
     title: "Brotzeit (gibt´s auch vegetarisch)",
     description:
-      "Knusprig gegrilltes Sandwich. Entweder mit Hähnchen, Peperoni-Salami oder vegetarisch. Alle Brote sind natürlich mit Käse belegt, weil mit Käse überbacken einfach alles besser schmeckt :)",
+      "Knusprig gegrillter Bagel. Entweder mit Hähnchen, Peperoni-Salami oder vegetarisch. Alle Brote sind natürlich mit Käse belegt, weil mit Käse überbacken einfach alles besser schmeckt :)",
     price: "6.50",
   },
   {
@@ -26,27 +46,6 @@ const mittagessen_speisen = [
     price: "klein 4.80",
     alt_one: "mittel 6.80",
     alt_two: "groß 8.80",
-  },
-  {
-    id: 5,
-    title: "Vital-Schüssel (gibts auch glutenfrei)",
-    description:
-      "Kunterbunte Bowl mit allem was du brauchst. Grünzeug, Protein, Kohlenhydrate und Gemüse. Wir toppen deine Schüssel wahlweise mit: Hähnchenstreifen, Räucherlachs, Thunfisch, Feta und Ei oder Tofu (vegan)",
-    price: "11.50",
-  },
-  {
-    id: 6,
-    title: "Vital-Tasche",
-    description:
-      "Mit gesundem Allerlei gefüllter Weizentortilla, gegrillt. Wir toppen deinen Wrap wahlweise mit: Hähnchenstreifen, Räucherlachs, Thunfisch, Feta und Ei oder Tofu (vegan)",
-    price: "11.50",
-  },
-  {
-    id: 7,
-    title: "Kartoffelpizza",
-    description:
-      "Überbackener Reibekuchen wahlweise mit: Veggie / Gemüse + Käse, Salami / Peperonisalami + Gemüse + Käse oder Tuna / Thunfisch + Gemüse + Käse ",
-    price: "6.90",
   },
 ];
 
